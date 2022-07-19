3c52d1184d861ab47df5ca38677b8a4def681582
HimaHina <blankov.boris@mail.ru>
2022-07-19 17:06:37 +0800

    .

--- bot.py
+++ bot.py
@@ -17,11 +17,12 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await bot.send_message(message.from_user.id, f"Hello, {message.from_user.first_name} Dattebayo!\n Сhoose the character you would like to watch series with.", reply_markup = markup.mainMenu)
+    await bot.send_message(message.from_user.id, f"Hello, {message.from_user.first_name} Dattebayo!\n Сhoose the character you would like to watch series with.", reply_markup=markup.mainMenu)
 
 @dp.message_handler()
 async def send_welcome(message: types.Message):
     if message.text == "Obito Uchiha":
+        picture = types.InputFile('media/picture/obito.jpg')
         cursor.execute("""SELECT number FROM series WHERE id == 1""")
         list = []
         while True:
@@ -32,9 +33,12 @@ async def send_welcome(message: types.Message):
 
             list.append(results[0])
 
+        await bot.send_photo(message.from_user.id, photo=picture)
         await bot.send_message (message.from_user.id, f"{list}")
+        await bot.send_message(message.from_user.id, "Do you want to choose another variant? Type 'yes' or 'no'.")
 
     elif message.text == "Itachi Uchiha":
+        picture = types.InputFile('media/picture/itachi.jpg')
         cursor.execute("""SELECT number FROM series WHERE id == 2""")
         list = []
         while True:
@@ -45,7 +49,18 @@ async def send_welcome(message: types.Message):
 
             list.append(results[0])
 
+        await bot.send_photo(message.from_user.id, photo=picture)
         await bot.send_message(message.from_user.id, f"{list}")
+        await bot.send_message(message.from_user.id, "Do you want to choose another variant? Type 'yes' or 'no'.")
+
+    elif message.text == 'yes':
+        await bot.send_message(message.from_user.id, "Here are the menu!", reply_markup=markup.mainMenu)
+
+    elif message.text == 'no':
+        await bot.send_message(message.from_user.id, "Enjoy watching the series!")
+
+    else:
+        await  bot.send_message(message.from_user.id, "I don't know this command :(. I'll call the menu so you can choose any available option.", reply_markup = markup.mainMenu)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- markup.py
+++ markup.py
@@ -3,4 +3,4 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 #Главное меню
 btnObito = KeyboardButton('Obito Uchiha')
 btnItachi = KeyboardButton('Itachi Uchiha')
-mainMenu = ReplyKeyboardMarkup(resize_keyboard= True).add(btnObito, btnItachi)
+mainMenu = ReplyKeyboardMarkup(resize_keyboard= True, one_time_keyboard= True).add(btnObito, btnItachi)
